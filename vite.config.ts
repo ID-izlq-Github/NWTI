28b4ac36e5aa40da72a0bdcdbea6e9abc186159f
Lequan_Zhang <zlq05741213@outlook.com>
2026-05-20 15:04:15 +0800

    新增了部署配置

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // 部署到 GitHub Pages 时，base 应设为 '/<repo名>/'（如 '/nwti/'）
-  // 本地开发时不需要此配置
-  // base: '/nwti/',
+  // 部署到 GitHub Pages 时，base 应设为 '/<repo名>/'（如 '/NWTI/'）
+  // 本地开发时不需要此配置 使用npm run dev 进行开发时，Vite 会自动处理路径问题
+  base: '/NWTI/',
   plugins: [react()],
 })
